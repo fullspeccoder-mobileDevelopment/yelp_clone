45ea47658f115e33d47832762eb266e5713696ee
Jacob Wilson <jcbwlsn04@gmail.com>
2024-09-03 15:59:31 -0500

    Added theme classes & theme observer to automatically change themes dark and light according to device changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:keiko_food/helper/themes.dart';
 import 'package:keiko_food/pages/home.dart';
 
 void main() {
@@ -12,10 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: Themes.lightTheme(),
+      darkTheme: Themes.darkTheme(),
+      themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
